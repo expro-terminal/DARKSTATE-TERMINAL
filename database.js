5a6f2cc102b538ffd4a9349dc6f1ad8caca5677e
expro-terminal <thugerator11@gmail.com>
2026-05-12 09:34:23 +0200

    Add files via upload

---
+++ database.js
@@ -0,0 +1,14 @@
+const database = {
+  "NODE-ALPHA": {
+    status: "ACTIVE",
+    link: "pages/node-alpha.html"
+  },
+  "NODE-BETA": {
+    status: "DEAD",
+    link: "pages/node-beta.html"
+  },
+  "NODE-GAMMA": {
+    status: "UNKNOWN",
+    link: "pages/node-gamma.html"
+  }
+};
